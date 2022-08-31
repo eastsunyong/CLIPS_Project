b00b4d02a094e0f46ae1ebf7c423159131777d14
somfist <qkrruddjs317@naver.com>
2022-08-31 17:09:31 +0900

    create pages, Router.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 
+import Router from "shared/Router";
 import Layout from "components/common/mobileLayout/Layout";
 import Main from "components/common/mobileLayout/Main";
 
 function App() {
   return (
     <Layout>
-      <Main></Main>
+      <Main>
+        <Router />
+      </Main>
     </Layout>
   );
 }

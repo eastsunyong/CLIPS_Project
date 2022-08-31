7dfd3c9ac5aebe912d7070a3d547ba2400c4bf23
qkrruddjs317@naver.com <>
2022-08-31 17:11:49 +0900

    create Footer.jsx

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Router from "shared/Router";
 import Layout from "components/common/mobileLayout/Layout";
 import Main from "components/common/mobileLayout/Main";
+import Footer from "components/common/mobileLayout/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Main>
         <Router />
       </Main>
+      <Footer />
     </Layout>
   );
 }

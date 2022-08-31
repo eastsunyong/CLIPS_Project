b077fcd76dd40c09c2a4b508922e432532cfe1f4
somfist <qkrruddjs317@naver.com>
2022-08-31 17:05:20 +0900

    create mobileLayout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import Layout from "components/common/mobileLayout/Layout";
+import Main from "components/common/mobileLayout/Main";
+
 function App() {
-  return <div></div>;
+  return (
+    <Layout>
+      <Main></Main>
+    </Layout>
+  );
 }
 
 export default App;

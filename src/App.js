fb3157aea0f5fc0191154a18a4f5a67d8652795b
qkrruddjs317@naver.com <>
2022-08-31 17:12:44 +0900

    fix mobileLayout 폴더위치 변경

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 import Router from "shared/Router";
-import Layout from "components/common/mobileLayout/Layout";
-import Main from "components/common/mobileLayout/Main";
-import Footer from "components/common/mobileLayout/Footer";
+import Layout from "components/mobileLayout/Layout";
+import Main from "components/mobileLayout/Main";
+import Footer from "components/mobileLayout/Footer";
 
 function App() {
   return (

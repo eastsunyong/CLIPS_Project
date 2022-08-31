f84372a8cae9fe093bd25ebcf035a38746613f7f
eastsunyong <sunyong9059@naver.com>
2022-08-31 19:13:31 +0900

    Roter.jsx 수정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "pages/Home";
 import Login from "pages/Login";
+import SignUp from "pages/SignUp";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
 };

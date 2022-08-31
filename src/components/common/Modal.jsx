0dcfa0beb5bad9b571783f2287723280f97f5d42
somfist <qkrruddjs317@naver.com>
2022-09-01 03:02:58 +0900

    create HomeRefac.jsx (리팩토링)

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -11,7 +11,9 @@ const Modal = (props) => {
 
 const Container = styled.div`
   position: absolute;
+  top: 0;
   left: ${(props) => (props.toggle ? "0%" : "100%")};
+  z-index: 10000;
   height: 100%;
   width: 100%;
   transition-duration: 1s;

43f0955a669b1260df9465ddf59fd601f5b92a12
qkrruddjs317@naver.com <>
2022-09-03 00:05:39 +0900

    create home_v2.jsx (디자인따른 리팩토링)

---
+++ src/pages/Home_v2.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home_v2 = () => {
+  return <div>Home_v2</div>;
+};
+
+export default Home_v2;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Home from "./Home";
+import Home_v2 from "./Home_v2";
 import Login from "./Login";
 import SignUp from "./SignUp";
 
-export { Home, Login, SignUp };
+export { Home, Login, SignUp, Home_v2 };

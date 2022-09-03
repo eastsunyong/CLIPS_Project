39f6522d16b725e5590238c87fb96338a33fff8c
eastsunyong <sunyong9059@naver.com>
2022-09-03 16:54:40 +0900

    update Login.jsx(로그인 조건 변경))

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,14 +6,19 @@ import { useState } from "react";
 import axios from "axios";
 
 const Login = () => {
-
+  
   const navigate = useNavigate();
 
+  //로그인 함수
+  const onSubmit = (data) => {
+    LogInHandler(data)
+  };
 
+
+  //로그핸들러
   const LogInHandler = async (data) => {
     await axios.post(process.env.REACT_APP_SURVER + '/api/auth/signup', data)
   }
-
 
   const {
     getValues,
@@ -23,47 +28,42 @@ const Login = () => {
     formState: { errors },
   } = useForm({ mode: "onChange" });
 
-  const onSubmit = (data) => {
-    // LogInHandler(data)
-    const Qwe = getValues("nickname")
-    console.log(data);
-    console.log(Qwe);
-  };
 
+  //오류 메세지 확인
   const onValid = (data) => console.log(data, "onvalid");
   const onInvalid = (data) => console.log(data, "onInvalid");
-
-
-  // {...register("email", {required: "이메일은 필수 입력입니다.",
-  // minLength: {value: 8,message: "이메일을 8자 이상 작성해주세요",},maxLength: {value: 30,message: "이메일을 30자 이하로 작성해주세요", },
-  //   pattern: {
-  //   value:
-  //   /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/,
-  //   message: "이메일이 형식에 맞지 않습니다.",
-  //   },
-  //   })}
-
-  // { pattern:/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/}
 
   return (
     <Container className="fcc">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <p>nickname</p>
+          <div style={{ flexDirection: "row", display: "flex", gap: "2rem" }}>
+            <label>이메일</label>
+            {errors.email && <p style={{ color: "red" }}>{errors.email.message}</p>}
+          </div>
           <input
-            {...register("nickname", { required: "빈칸입니다", minLength: { value: 5, message: "아이디가 짧아여", } })}
-            placeholder="닉네임" />
-          {errors.nickname && <p style={{ color: "red" }}>{errors.nickname.message}</p>}
-
-          <p>비밀번호</p>
-          <input
-            {...register("password", { required: "비밀번호는 필수 입력입니다.", minLength: { value: 8, message: "8자리 이상 비밀번호를 사용하세요.", }, })}
-            placeholder="비밀번호"
-            type="password"/>
-          {errors.password && <p style={{ color: "red" }}>{errors.password.message}</p>}
-
-
+            {...register("email", {
+              required: "이메일은 필수 입력입니다", maxLength: { value: 30, message: "30자 이하로 정해주세요" },
+              pattern: {
+                value: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/, message: "이메일이 형식에 맞지 않습니다.",
+              }
+            })}
+            placeholder="이메일"
+          />
         </div>
+        <div style={{ flexDirection: "row", display: "flex", gap: "2rem" }}>
+          <label>비밀번호</label>
+          {errors.password && <p style={{ color: "red" }}>{errors.password.message}</p>}
+        </div>
+        <input
+          {...register("password", {
+            required: "비밀번호는 필수 입력입니다", minLength: { value: 8, message: "8자리 이상 비밀번호를 사용하세요.", },
+            maxLength: { value: 16, message: "16자리 이하 비밀번호를 사용하세요.", }, pattern: { value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/, message: "특수문자와 숫자를 포함해주세요" }
+          })}
+          placeholder="비밀번호 (특수문자, 숫자 포함)"
+          type="password"
+        />
+
         <div className="fcc">
           <Container>
             <button>로그인</button>
@@ -80,7 +80,7 @@ const Container = styled.div`
 
 flex-direction: column;
   p{
-    font-size: 2rem;
+    font-size: 12px;
   }
   input {
     height: 3rem;

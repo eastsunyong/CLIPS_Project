86f1029687a6daf3442b933d730d1f242e550170
eastsunyong <sunyong9059@naver.com>
2022-09-07 15:56:37 +0900

    update Router.jsx (경로 추가)

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import { Home, Logins } from "pages";
+import { Home, Logins, Promised } from "pages";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Logins />} />
+      <Route path="/Promised" element={<Promised />} />
     </Routes>
   );
 };

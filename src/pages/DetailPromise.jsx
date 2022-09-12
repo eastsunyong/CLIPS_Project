ede7786b38840ed2647ae2f4af36661f301775b6
eastsunyong <sunyong9059@naver.com>
2022-09-12 23:15:33 +0900

    create DetailPromise.jsx 생성 (약속 상세 페이지 생성) update index.js, Router.jsx 수정 (라우터 경로 추가)

---
+++ src/pages/DetailPromise.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DetailPromise = () => {
+    return(
+        <div>
+            약속 상세 페이지
+            테스트입니다
+        </div>
+    )
+}
+
+export default DetailPromise

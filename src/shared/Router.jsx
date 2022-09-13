21025e561ab0d6dfabd1fb82b4c0345fc2053ab6
eastsunyong <sunyong9059@naver.com>
2022-09-13 18:45:04 +0900

    create ReviewList.jsx, Mypage.jsx, index.js 생성 (마이페이지 컴포넌트 생성), update Router.js 수정 (경로 추가)

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import { Home, Logins, Promised, DetailPromise } from "pages";
+import { Home, Logins, Promised, DetailPromise, Mypage } from "pages";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Logins />} />
+      <Route path="/mypage" element={<Mypage />} />
       <Route path="/Promised" element={<Promised />} />
       <Route path="/Promise/:promiseId" element={<DetailPromise />} />
     </Routes>

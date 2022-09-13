57eefec1720c6b6b7819be41ec836b8c24bd2543
eastsunyong <sunyong9059@naver.com>
2022-09-13 18:45:04 +0900

    create ReviewList.jsx, Mypage.jsx, index.js 생성 (마이페이지 컴포넌트 생성), update Router.js 수정 (경로 추가)

---
+++ src/components/mypage/ReviewList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ReviewList = ()=> {
+    return (
+        <div>
+            리뷰 가져오기
+        </div>
+    )
+}
+
+export default ReviewList
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,6 @@ import Home from "./Home";
 import Logins from "./Login";
 import Promised from "./Promised";
 import DetailPromise from "./DetailPromise";
+import Mypage from "./Mypage";
 
-export { Home, Logins, Promised, DetailPromise };
+export { Home, Logins, Promised, DetailPromise, Mypage };

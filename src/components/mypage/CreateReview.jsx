85587cfefb115304facb22e6d86b9fbaa0812d52
eastsunyong <sunyong9059@naver.com>
2022-09-14 00:02:55 +0900

    Create CreateReview.jsx, LoadReview.jsx 생성 (컴포넌트 생성), update index.js 수정 (경로 추가)

---
+++ src/components/mypage/CreateReview.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateReview = ()=> {
+    return (
+        <div>
+            후기 만들기임
+        </div>
+    )
+}
+
+export default CreateReview
---
+++ src/components/mypage/LoadReview.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadReview = ()=> {
+    return (
+        <div>
+            작성한 후기임
+        </div>
+    )
+}
+
+export default LoadReview

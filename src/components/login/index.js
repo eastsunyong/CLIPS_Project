faba13c01d3480d6b09eaa43bec0ebc3c8189259
eastsunyong <sunyong9059@naver.com>
2022-09-14 11:59:55 +0900

    update SignUpPage.jsx, SignUpPage.jsx2, SignUpPage.jsx3 수정 (와이어 프레임 변경으로 인한 수정)

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -5,5 +5,7 @@ import Choice from "./Choice";
 import FindPassword from "./FindPassword";
 import FindId from "./FindId";
 import Line from "./Line";
+import SignUpPage2 from "./SignUpPage2";
+import SignUpPage3 from "./SignUpPage3";
 
-export {Indroduce, LoginPage, SignUpPage, Choice, FindId, FindPassword, Line}
+export {Indroduce, LoginPage, SignUpPage, Choice, FindId, FindPassword, Line, SignUpPage2, SignUpPage3}

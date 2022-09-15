0f912d187d27276bbdc9f9dcb6da706b9df3f8ab
eastsunyong <sunyong9059@naver.com>
2022-09-15 15:50:36 +0900

    커밋하면 actions가 테스트 및 압축 후 aws s3에 배포

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>커밋하면 actions가 테스트 및 압축 후 aws s3에 배포</p>
     </div>
   );
 }

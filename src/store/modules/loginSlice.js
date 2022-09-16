1002bf9bee26bf7b1470b503b6a082c4156cc80a
qkrruddjs317@naver.com <>
2022-09-16 21:58:34 +0900

    comment 충돌 해결2

--- src/store/modules/loginSlice.js
+++ src/store/modules/loginSlice.js
@@ -1,7 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-<<<<<<< HEAD
   login: false,
 };
 
@@ -17,23 +16,3 @@ export const loginSlice = createSlice({
 
 export const { isLogin } = loginSlice.actions;
 export default loginSlice.reducer;
-=======
-    page: false
-};
-
-export const loginSlice = createSlice({
-    name: "LOGIN",
-    initialState,
-    reducers: {
-        addNumber: (state, action) => {
-            state.page =  action.payload
-        },
-        minusNumber: (state, action) => {
-            state.page = state.page - action.payload
-        },
-    },
-});
-
-export const { addNumber, minusNumber } = loginSlice.actions;
-export default loginSlice.reducer;
->>>>>>> 8d3e2b4d47a3322814312123280d3887a4375aa2

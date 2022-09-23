5f6e1b46f3ba1d3ed7242fc2035df89f1991fb93
qkrruddjs317@naver.com <>
2022-09-23 19:14:29 +0900

    comment 영업시간 활성화

--- src/components/page/main/PlaceInfo.jsx
+++ src/components/page/main/PlaceInfo.jsx
@@ -85,7 +85,7 @@ const PlaceInfo = (props) => {
           <span className="icon">
             <LocationIcon />
           </span>
-          {/* <span>{crawlData?.rawArrDateUrl[0] ? crawlData?.rawArrDateUrl[0] : "영업시간을 확인할 수 없습니다!"}</span> */}
+          <span>{crawlData?.rawArrDateUrl[0] ? crawlData?.rawArrDateUrl[0] : "영업시간을 확인할 수 없습니다!"}</span>
         </div>
       </Info>
       <Btn outLine={true} onClick={savePlace}>
